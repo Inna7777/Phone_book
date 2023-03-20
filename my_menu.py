90b3fc086447d36ea00ea03da58940af055e2ec5
Inna7777 <peshkova-inna@mail.ru>
2023-03-20 17:37:26 +0400

    Output programs added to the menu changed

--- my_menu.py
+++ my_menu.py
@@ -11,3 +11,23 @@ def menu():
 
     choice = input('Введите номер действия:')
     return choice
+
+
+def print_success(surname):
+    print('Контакт {} добавлен.'.format(surname))
+
+
+def print_contacts(contacts):
+    if len(contacts) == 0:
+        print('Контакт не найден')
+    else:
+        print('Найдены  контакты:')
+        for contact in contacts:
+            print(contact)
+
+
+def print_delete(found, surname):
+    if found == False:
+        print('Контакт не найден')
+    else:
+        print('Контакт {} удален.'.format(surname))
